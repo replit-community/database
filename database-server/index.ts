779e9ce1810fdf7e9e7682e135f5adc5f30a153c
Nathan Pham <nathanpham.me@gmail.com>
2023-05-12 21:58:05 -0700

    [:bust_in_silhouette:] Basic authentication

--- database-server/index.ts
+++ database-server/index.ts
@@ -1,40 +1,39 @@
 import * as dotenv from "dotenv";
 dotenv.config();
 
-import Fastify from "fastify";
-import S from "fluent-json-schema";
+import Fastify, { FastifyPluginCallback } from "fastify";
+import mongoose from "mongoose";
+
+import { createUser } from "./v1/routes/userRoutes/createUser";
+import { loginUser } from "v1/routes/userRoutes/loginUser";
 
 const app = Fastify();
 
-/*
-server methods
-- create account
-    - sign into account
-    - log out of account
-
-- create bin
-    - delete bin
-    - update bin title, description, allowed hosts
-    - get bin title, description, allowed hosts
-    - create bin API key with scopes (READ, WRITE)
-    - delete bin API key
-    - get API key & scopes
-
-- create bin key
-    - get bin key
-    - set bin key
-    - delete bin key
-*/
-
-app.get("/", async (req, res) => {
-    return {
-        success: true,
-    };
-});
-
-async function main(port = 3000, host = "0.0.0.0") {
-    await app.listen({ port, host });
-    console.log("database-server started");
+/**
+ * Register an array of routes under a given prefix
+ * @param routes Array of routes to register
+ * @param prefix Prefix, like v1
+ */
+function registerRoutes(routes: Array<FastifyPluginCallback>, prefix: string) {
+    for (const route of routes) {
+        app.register(route, { prefix });
+    }
 }
+
+/**
+ * Start Fastify server
+ * @param port Port to use
+ * @param host Host to use
+ */
+async function main(port = 3000, host = "0.0.0.0") {
+    await mongoose.connect(
+        `mongodb+srv://${process.env.MONGO_DB_USERNAME}:${process.env.MONGO_DB_PASSWORD}@repl-db-cluster-0.g7ydqoh.mongodb.net/?retryWrites=true&w=majority`
+    );
+    await app.listen({ port, host });
+
+    console.log(`database-server started on port ${port}`);
+}
+
+registerRoutes([createUser, loginUser], "v1");
 
 main();
